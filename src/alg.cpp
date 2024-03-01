84b0585f03e0d4114c5979d784c47490ceb86e46
IanaVatagina <vatagina.iana@gmail.com>
2024-03-01 21:09:07 +0300

    Commit

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,7 +11,7 @@ double pown(double value, uint16_t n) {
 }
 
 uint64_t fact(uint16_t n) {
-    uint16_t result = 1;
+    uint64_t result = 1;
     for (uint64_t i=1; i <= n; i++) {
         result *= i;
     }
