f64dd460fc34f98016819d84b9871fcf9a8015b5
IanaVatagina <vatagina.iana@gmail.com>
2024-03-04 19:06:03 +0300

    Commit

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,7 +3,7 @@
 
 
 double pown(double value, uint16_t n) {
-    long double result = 1;
+    double result = 1;
     for (uint64_t i=1; i <= n; i++) {
         result *= value;
     }
@@ -41,7 +41,7 @@ double sinn(double x, uint16_t count) {
 double cosn(double x, uint16_t count) {
     double result = 0.0;
     for (uint64_t i = 1; i <= count; i++) {
-        result += calcItem(x, 2 * i)*pown(-1, i);
+        result += calcItem(x, 2 * i - 2)*pown(-1, i-1);
     }
     return result;
 }
